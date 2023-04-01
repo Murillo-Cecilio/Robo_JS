3ca2579d7c0634e5c11271f62888c9f7e45f3bad
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-01 16:23:13 -0300

    tentando resolver array que não funcionou

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,7 @@ const subtrair = document.querySelector('#subtrair')
 const somar = document.querySelector('#somar')
 const braço = document.querySelector('#braço')
 
+const controle = document.querySelector('.controle-ajuste')
 
 // const robotron = document.querySelector('#robotron')
 
@@ -15,10 +16,15 @@ const braço = document.querySelector('#braço')
 //     console.log('Bem vindo ao Robo 2023')
 // }
 
-somar.addEventListener('click', (evento) => {
-    braço.value = parseInt(braço.value) + 1
-})
 
-subtrair.addEventListener('click', (evento) => {
-    braço.value = parseInt(braço.value) - 1
-})
+somar.addEventListener('click', () => { manipulaDados('somar') })//deixando o código reaproveitado para demais comandos da página
+
+subtrair.addEventListener('click', () => { manipulaDados('subtrair') })
+
+function manipulaDados(operação) {
+    if (operação === "subtrair") {
+        braço.value = parseInt(braço.value) - 1
+    } else {
+        braço.value = parseInt(braço.value) + 1
+    }
+}
