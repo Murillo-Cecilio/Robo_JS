9850f269d8d90423c79ccf7ec5704804c93090d0
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-01 15:37:10 -0300

    adicionando soma e subtração no robo

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,24 @@
-const robotron = document.querySelector('#robotron')
-robotron.addEventListener('click', dizOi) //add
+const subtrair = document.querySelector('#subtrair')
+const somar = document.querySelector('#somar')
+const braço = document.querySelector('#braço')
 
-function dizOi() {
-    console.log('oi')
-}
+
+// const robotron = document.querySelector('#robotron')
+
+// // Funções anônima, só vai funcionar quando o evento do click for disparado, 
+// se utilizar () => tbm funciona como arrow function
+// obotron.addEventListener('click', (evento) => {
+//     console.log(evento)
+// })
+// function dizOi(nome) {//função nomeada(declaração de uma função)
+//     console.log("oi" + nome)
+//     console.log('Bem vindo ao Robo 2023')
+// }
+
+somar.addEventListener('click', (evento) => {
+    braço.value = parseInt(braço.value) + 1
+})
+
+subtrair.addEventListener('click', (evento) => {
+    braço.value = parseInt(braço.value) - 1
+})
