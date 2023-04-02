49454702f78cfbfc5537bb1effbfdeeec1d3dea9
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-02 13:49:01 -0300

    realizado data-attributes

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,29 @@
 const subtrair = document.querySelector('#subtrair')
 const somar = document.querySelector('#somar')
-const braço = document.querySelector('#braço')
 //erro na linha 5 foi justamente por não usar SelectorAll para utilizar array tem que ter multiplos comandos dentro dele
-const controle = document.querySelectorAll('.controle-ajuste')
+const controle = document.querySelectorAll('[data-controle]')//dentro do [] é data attribute
 //linha 7 array formado para somar e subtrair todos os status
 controle.forEach((elemento) => {
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 });
+
+function manipulaDados(operação, controle) {
+    const peca = controle.querySelector('[data-contador]')
+
+    if (operação === "-") {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
 
 
 // const robotron = document.querySelector('#robotron')
 
-// // Funções anônima, só vai funcionar quando o evento do click for disparado, 
+// // Funções anônima, só vai funcionar quando o evento do click for disparado,
 // se utilizar () => tbm funciona como arrow function
 // obotron.addEventListener('click', (evento) => {
 //     console.log(evento)
@@ -27,11 +37,3 @@ controle.forEach((elemento) => {
 // somar.addEventListener('click', () => { manipulaDados('somar') })//deixando o código reaproveitado para demais comandos da página
 
 // subtrair.addEventListener('click', () => { manipulaDados('subtrair') })
-
-function manipulaDados(operação) {
-    if (operação === "-") {
-        braço.value = parseInt(braço.value) - 1
-    } else {
-        braço.value = parseInt(braço.value) + 1
-    }
-}
